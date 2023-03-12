b052507e151192ebbd3ad090fa803025835ae4ca
Harsh chaudhari <hchaudhari007@gmail.com>
2023-03-12 23:01:05 +0530

    First Commit

---
+++ src/main/java/com/example/FlightJet/FlightJetApplication.java
@@ -0,0 +1,13 @@
+package com.example.FlightJet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightJetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightJetApplication.class, args);
+	}
+
+}
